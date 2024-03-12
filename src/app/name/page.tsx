e31da1d1dbed9916a69853718559881f81ed2ac2
moriken <moriken.tech@gmail.com>
2024-03-13 00:47:12 +0900

    404

---
+++ src/app/name/page.tsx
@@ -0,0 +1,11 @@
+export default function Name() {
+    return(
+        <main>
+            <h1 className="title">Name page</h1>
+            <p className="msg">/name/◯◯ というように名前をつけてアクセスしてください。</p>
+            <div>
+                <a href="/">go back!!</a>
+            </div>
+        </main>
+    )
+}
