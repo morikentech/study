4708a162237db71f8287834952902ddeab1fe519
moriken <moriken.tech@gmail.com>
2024-03-13 00:33:46 +0900

    image

--- src/app/other/page.tsx
+++ src/app/other/page.tsx
@@ -1,10 +1,14 @@
 import Link from "next/link"
+import Image from "next/image"
 
 export default function Other() {
     return(
         <main>
             <h1 className="title">Other page</h1>
             <p className="msg">これは、別のページです。</p>
+            <div>
+                <Image src="/sample.jpg" width={200} height={200} />
+            </div>
             <div>
                 <Link href="/">go back!!</Link>
             </div>
