794b06f63ef43f0b24d7fcb350cec8eaae151683
moriken <moriken.tech@gmail.com>
2024-03-14 21:32:45 +0900

    jsx

--- src/app/other/page.tsx
+++ src/app/other/page.tsx
@@ -1,12 +1,23 @@
+'use client'
+
 import Link from "next/link"
 import Image from "next/image"
-import "./style.module.css"
+import style from "../style/style.module.css"
+import JSXStyle from "styled-jsx/style"
 
 export default function Other() {
     return(
         <main>
-            <h1 className="title">Other page</h1>
-            <p className="msg">これは、別のページです。</p>
+            <JSXStyle>
+                {`p.jsx-msg{
+                    margin : 10px;
+                    text-align : center;
+                    color: red;
+                    font-weigt: bold;
+                }`}
+            </JSXStyle>
+            <h1 className={styles.title}>Other page</h1>
+            <p className="jsx-msg">これは、別のページです。</p>
             <div>
                 <Image src="/sample.jpg" width={200} height={200} />
             </div>
