61c0a6dfbc942e377ab50663bd1a02dd9b4e0356
moriken <moriken.tech@gmail.com>
2024-03-18 00:10:51 +0900

    gsp

--- src/app/name/[name]/page.tsx
+++ src/app/name/[name]/page.tsx
@@ -1,11 +1,36 @@
-export default function Name({params}:{params:{name: string}}) {
+
+"use server"
+
+const paths = [
+    {name: "taro"},
+    {name: "hanako"},
+    {name: "sachiko"}
+]
+
+export async function generateStaticParams() {
+    return paths
+}
+
+export default async function Name({params}) {
+    const result = paths.some(path=>path.name===params.name)
+
     return (
         <main>
-            <h1 className="title">Other page</h1>
-            <p className="msg">あなたは、「{params.name}ですね。」</p>
+            {result ?
+            <>
+            <h1 className="title">Name = &quot{params.name}&quot</h1>
+            <p className="msg">{params.name}さん、こんにちは！</p>
+            </>
+            :
+            <>
+            <h1 className="title">&quot{params.name}&quot</h1>
+            <p className="msg">「{params.name}」は使えません。</p>
+            </>
+            }
+
         <div>
             <a href="/">go back!!</a>
         </div>
-        </main>
+     </main>
     )
 }
