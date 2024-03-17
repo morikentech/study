5aa825068cdd9651a2c1014c23b25e17cc34a837
moriken <moriken.tech@gmail.com>
2024-03-17 23:53:19 +0900

    app server

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,22 @@
-import Link from "next/link"
+"use server"
+import { Metadata } from "next"
 
-export default function Home() {
+export async function generateMetadata(){
+  return{
+    title: "Index page",
+  }
+}
+
+const defaultProps = {
+  title:"Static page",
+  msg:"This is static page sample."
+}
+
+export default async function Home() {
    return (
     <main>
-      <h1 className="title">Top page</h1>
-      <p className="msg">This is other page sample.</p>
-      <div>
-        <Link href="/other">go other page</Link>
-      </div>
+      <h1 className="title">{defaultProps.title}</h1>
+      <p className="msg">{defaultProps.msg}</p>
     </main>
   )
 }
